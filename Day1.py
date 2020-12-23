d06ea2a9c7c5950d147302442be7319313fd2894
= <liam.percy@roguetwo.tech>
2020-12-24 00:29:19 +1000

    Abstracting and adding faster method for 2 pairs

--- Day1.py
+++ Day1.py
@@ -1,5 +1,46 @@
 import itertools
 import numpy
+import time
+
+def solutionBrute(iterations):
+    # For every combination of numbers, find where the two 
+    # equal 2020 and then multiply them together. Print
+    # the result
+    tic = time.perf_counter()
+    for numbers in itertools.combinations(input, iterations): 
+        if sum(numbers) == 2020:
+            toc = time.perf_counter()
+            # The two numbers
+            print(f"Numbers: {numbers}")
+
+            # Confirmation
+            print(f"The sum of these numbers is: {sum(numbers)}")
+
+            # Their Index
+            print(f"Index: {[input.index(number) for number in numbers]}")
+
+            # Answer
+            print(f"Answer {numpy.prod(numbers)}") 
+
+            print(f"It took {toc - tic:0.6f} seconds to find this solution")
+
+def solution():
+    target = 2020
+    halfTarget = 2020//2
+
+    tic = time.perf_counter()
+
+    # Find values of x such that 2020 - x is also in the list
+    validSets = {target-x for x in input if x<=halfTarget} & {x for x in input if x>halfTarget}
+    pairs = {(target-x, x) for x in validSets}
+
+    toc = time.perf_counter()
+
+    print(f"Numbers using faster solution: {pairs}")
+
+    print(f"It took {toc - tic:0.6f} seconds to find this solution")
+
+
 
 input = [1973,
 1755,
@@ -202,41 +243,15 @@ input = [1973,
 1726,
 1364]
 
-
 # PART 1
+print("\nSolution to Part 1:")
+solutionBrute(2)
 
-# For every combination of numbers, find where the two 
-# equal 2020 and then multiply them together. Print
-# the result
-for numbers in itertools.combinations(input, 2): 
-    if sum(numbers) == 2020:
-        # The two numbers
-        print(f"Numbers: {numbers}")
 
-        # Confirmation
-        print(f"The sum of these numbers is: {sum(numbers)}")
-
-        # Their Index
-        print(f"Index: {[input.index(number) for number in numbers]}")
-
-        # Answer
-        print(f"Answer {numpy.prod(numbers)}")
+print("\nSolution to Part 1 Using Set Logic:")
+solution()
 
 
 # PART 2
-# For every 3 combination of numbers, find where the two 
-# equal 2020 and then multiply them together. Print
-# the result
-for numbers in itertools.combinations(input, 3): 
-    if sum(numbers) == 2020:
-        # The two numbers
-        print(f"Numbers: {numbers}")
-
-        # Confirmation
-        print(f"The sum of these numbers is: {sum(numbers)}")
-
-        # Their Index
-        print(f"Index: {[input.index(number) for number in numbers]}")
-
-        # Answer
-        print(f"Answer {numpy.prod(numbers)}")
+print("\nSolution to Part 2:")
+solutionBrute(3)
